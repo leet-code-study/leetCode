5996931910109816996562b2f3666d1b4106f90f
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-04-01 20:39:59 +0900

    redandsilver-1
    for문 이용

---
+++ src/main/java/com/example/redansilver/Twosum_1.java
@@ -0,0 +1,19 @@
+package com.example.redansilver;
+
+class Twosum_1 {
+  public int[] twoSum(int[] nums, int target) {
+    int[] answer = new int[2];
+    for(int i=0; i<nums.length; i++){
+      int b = target - nums[i];
+      for(int j =1; j<nums.length; j++){
+        if( b == nums[j] && i != j){
+          answer[0] = i;
+          answer[1] = j;
+          break;
+        }
+      }
+
+    }
+    return answer;
+  }
+}
