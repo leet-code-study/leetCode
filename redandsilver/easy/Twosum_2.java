bda80eaa06a9581b51cb1294c2481010bf347574
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-04-03 20:07:21 +0900

    redandsilver-2
    hashmap 사용

---
+++ redandsilver/easy/Twosum_2.java
@@ -0,0 +1,22 @@
+package redandsilver.easy;
+
+import java.util.HashMap;
+
+class Twosum_2 {
+  public int[] twoSum(int[] nums, int target) {
+    int[] answer = new int[2];
+    HashMap<Integer,Integer> map = new HashMap<>();
+    for(int i=0; i<nums.length; i++){
+      map.put(nums[i], i);
+    }
+    for(int i=0; i<nums.length; i++){
+      int number = target - nums[i];
+      if(map.containsKey(number) && map.get(number) != i){
+        answer[0] = i;
+        answer[1] = map.get(number);
+        break;
+      }
+    }
+    return answer;
+  }
+}
