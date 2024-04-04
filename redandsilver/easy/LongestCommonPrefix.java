33b494ef7536a951e04b3137c08df977b71119f3
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-04-04 20:10:36 +0900

    redandsilver-3

---
+++ redandsilver/easy/LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+package redandsilver.easy;
+
+import java.util.Arrays;
+
+class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String f = strs[0];
+        String l = strs[strs.length-1];
+        StringBuilder answer = new StringBuilder();
+        int n = Math.min(f.length(),l.length());
+        for(int i=0; i<n; i++){
+            if(f.charAt(i) != l.charAt(i)){
+                return answer.toString();
+            }
+            answer.append(f.charAt(i));
+        }
+        return answer.toString();
+    }
+}
