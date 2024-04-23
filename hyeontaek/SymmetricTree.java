dcf3d25f94f2f7ad1e02f178547c60c160861e65
limHyeonTaek <lht990e@naver.com>
2024-04-23 11:18:19 +0900

    hyeontaek-20

---
+++ hyeontaek/SymmetricTree.java
@@ -0,0 +1,33 @@
+package hyeontaek;
+
+public class SymmetricTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    public static void main(String[] args) {
+        SymmetricTree solution = new SymmetricTree();
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+        System.out.println(solution.isSymmetric(root));
+    }
+
+}
