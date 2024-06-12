cdf2a6e7360ce4b89f4837a4512ecc51b8755e73
limHyeonTaek <lht990e@naver.com>
2024-06-12 15:12:09 +0900

    hyeontaek-59

--- hyeontaek/MoveZeroes.java
+++ hyeontaek/MoveZeroes.java
@@ -1,25 +1,26 @@
 package hyeontaek;
 
 public class MoveZeroes {
-    public void moveZeroes(int[] nums) {
-        int idx = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[idx++] = nums[i];
-            }
-        }
-        for (int i = idx; i < nums.length; i++) {
-            nums[i] = 0;
-        }
-    }
 
-    public static void main(String[] args) {
-        MoveZeroes solution = new MoveZeroes();
-        int[] nums = {0, 1, 0, 3, 12};
-        solution.moveZeroes(nums);
-        for (int num : nums) {
-            System.out.printf(num + " ");
-        }
+  public static void main(String[] args) {
+    MoveZeroes solution = new MoveZeroes();
+    int[] nums = {0, 1, 0, 3, 12};
+    solution.moveZeroes(nums);
+    for (int num : nums) {
+      System.out.printf(num + " ");
     }
+  }
+
+  public void moveZeroes(int[] nums) {
+    int idx = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        nums[idx++] = nums[i];
+      }
+    }
+    for (int i = idx; i < nums.length; i++) {
+      nums[i] = 0;
+    }
+  }
 
 }
