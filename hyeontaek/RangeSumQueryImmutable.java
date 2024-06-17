f156999a2970902b07aaa544e53af227e92a190b
limHyeonTaek <lht990e@naver.com>
2024-06-17 13:56:11 +0900

    hyeontaek-62

---
+++ hyeontaek/RangeSumQueryImmutable.java
@@ -0,0 +1,21 @@
+package hyeontaek;
+
+public class RangeSumQueryImmutable {
+
+}
+
+class NumArray {
+    private final int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
